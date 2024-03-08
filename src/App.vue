810260c6912a4d4cde04b9a11faa6bf95af464b4
fuwedong <fuwendong5@outlook.com>
2024-03-08 11:16:01 +0800

    feat: 修改基础样式

--- src/App.vue
+++ src/App.vue
@@ -30,6 +30,7 @@
           colorWarning: '#EFBD47',
           colorError: '#ED6F6F',
           colorInfo: '#0960bd',
+          borderRadius: 2,
         },
       },
       isDark.value ? darkTheme : {},
